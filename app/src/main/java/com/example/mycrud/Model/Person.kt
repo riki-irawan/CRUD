1482365d0faa87983c8eaf3eb0d1093a3b001407
riki-irawan <riki.irawan@students.amikom.ac.id>
2021-07-19 12:36:11 +0700

    commit crud

---
+++ app/src/main/java/com/example/mycrud/Model/Person.kt
@@ -0,0 +1,14 @@
+package com.example.mycrud.Model
+
+class Person {
+    var id:Int=0
+    var name:String?=null
+    var email:String?=null
+    constructor(){}
+    constructor(id:Int,name:String,email:String)
+    {
+        this.id=id
+        this.name=name
+        this.email=email
+    }
+}
